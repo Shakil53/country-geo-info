fe070cc22d17be0f8f5fa4908807b4e0888ff894
Shakil53 <shakilahmed.dcc@gmail.com>
2022-11-10 12:21:11 +0600

    new

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const API_KEY = `233daba8fcmsh18b28f632bf7100p19b650jsn212834482dbd`;
 
-https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions
+const searchCountry = () => {
+    const city = document.getElementById('input').value;
+    const url = `https://wft-geo-db.p.rapidapi.com/v1&${city}&${API_KEY}`;
+    
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
